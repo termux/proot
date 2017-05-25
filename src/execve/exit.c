fc1f2daaf11140acffdd4d438ab5148529001efe
Michał Bednarski <bednarski.michal2@gmail.com>
2017-05-25 15:03:28 +0200

    Leave thumb mode exiting from loader under ptrace
    Make sure that after execve() under emulated ptrace thumb mode
    is not active even if loader was in thumb
    
    Leaving loader in traced process is separate code path from untraced
    one, handling of untraced process was fixed in a01472

--- src/execve/exit.c
+++ src/execve/exit.c
@@ -419,6 +419,11 @@ void translate_execve_exit(Tracee *tracee)
 		poke_reg(tracee, RTLD_FINI, 0);
 		poke_reg(tracee, STATE_FLAGS, 0);
 
+#if defined(ARCH_ARM_EABI) && defined(__thumb__)
+		/* Leave ARM thumb mode */
+		tracee->_regs[CURRENT].ARM_cpsr &= ~PSR_T_BIT;
+#endif
+
 		/* Restore registers to their current values.  */
 		save_current_regs(tracee, ORIGINAL);
 		tracee->_regs_were_changed = true;
