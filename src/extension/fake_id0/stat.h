6ad40d651292fdc1300ba3655b0f11a266c918e2
Corbin Champion <corbinlc@gmail.com>
2018-08-31 12:29:15 -0700

    now compiling with all major pieces merged

--- src/extension/fake_id0/stat.h
+++ src/extension/fake_id0/stat.h
@@ -6,6 +6,11 @@
 #include "extension/fake_id0/config.h"
 
 int handle_stat_enter_end(Tracee *tracee, Reg fd_sysarg);
+#ifndef USERLAND
 int handle_stat_exit_end(Tracee *tracee, Config *config, Reg stat_sysarg);
+#endif
+#ifdef USERLAND
+int handle_stat_exit_end(Tracee *tracee, Config *config, word_t sysnum);
+#endif
 
 #endif /* FAKE_ID0_STAT_H */
