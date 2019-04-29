d3fc46870910f8a32712b6b4d3aaf6dd0b4c2819
Michal Bednarski <bednarski.michal2@gmail.com>
2019-04-29 18:55:26 +0200

    Add support for nested chroot
    We now support nested chroot() if new root
    can replace old while removing all bind mounts
    
    chroot(/) is supported as before without changes
    
    Added handling of chroot() if it's denied by seccomp
    
    termux/proot#34

--- src/extension/fake_id0/chroot.c
+++ src/extension/fake_id0/chroot.c
@@ -1,4 +1,5 @@
 #include <errno.h>       /* E*, */
+#include <sys/stat.h>    /* stat, */
 
 #include "tracee/reg.h"
 #include "tracee/mem.h"
@@ -6,35 +7,103 @@
 #include "path/binding.h"
 #include "extension/fake_id0/chroot.h"
 
-int handle_chroot_exit_end(Tracee *tracee, Config *config) {
+int handle_chroot_exit_end(Tracee *tracee, Config *config, bool from_sigsys) {
 	char path[PATH_MAX];
-	char path_absolute[PATH_MAX];
+	char path_guest[PATH_MAX];
+	char path_host_absolute[PATH_MAX];
 	word_t input;
 	int status;
 	word_t result;
+	struct stat statbuf;
+	bool seen_bind_under_new_root = false;
 
 	if (config->euid != 0) /* TODO: && !HAS_CAP(SYS_CHROOT) */
-		return 0;
+		return from_sigsys ? -EPERM : 0;
 
-	/* Override only permission errors.  */
-	result = peek_reg(tracee, CURRENT, SYSARG_RESULT);
-	if ((int) result != -EPERM)
-		return 0;
+	if (from_sigsys) {
+		/* Fetch reg early and from CURRENT RegVersion
+		 * if this call is from SIGSYS handler.  */
+		input = peek_reg(tracee, CURRENT, SYSARG_1);
 
-	input = peek_reg(tracee, MODIFIED, SYSARG_1);
+		/* Set default error if we're here due to SIGSYS.  */
+		poke_reg(tracee, SYSARG_RESULT, -EPERM);
+	} else {
+		/* Override only permission errors.
+		 * (If we're here due to SIGSYS we don't check that
+		 * we know syscall couldn't even be made.  */
+		result = peek_reg(tracee, CURRENT, SYSARG_RESULT);
+		if ((int) result != -EPERM)
+			return 0;
+	}
 
-	status = read_path(tracee, path, input);
+	/* Get chroot target path translated to host.  */
+	if (!from_sigsys) {
+		input = peek_reg(tracee, MODIFIED, SYSARG_1);
+
+		status = read_path(tracee, path, input);
+	} else {
+		status = read_path(tracee, path_guest, input);
+		if (status < 0)
+			return status;
+		status = translate_path(tracee, path, AT_FDCWD, path_guest, true);
+	}
+
 	if (status < 0)
 		return status;
 
-	realpath(path, path_absolute);
+	realpath(path, path_host_absolute);
 
-	/* Only "new rootfs == current rootfs" is supported yet.  */
-	status = compare_paths(get_root(tracee), path_absolute);
-	if (status != PATHS_ARE_EQUAL)
+	/* Handle "new rootfs == current rootfs" case.  */
+	status = compare_paths(get_root(tracee), path_host_absolute);
+	if (status == PATHS_ARE_EQUAL) {
+		/* Force success.  */
+		if (from_sigsys) return 1;
+		poke_reg(tracee, SYSARG_RESULT, 0);
 		return 0;
+	}
 
-	/* Force success.  */
-	poke_reg(tracee, SYSARG_RESULT, 0);
-	return 0;
+	/* Validate chroot target.  */
+	status = stat(path_host_absolute, &statbuf);
+	if (status < 0)
+		return -errno;
+
+	if (!S_ISDIR(statbuf.st_mode))
+		return -ENOTDIR;
+
+	/* Fetch guest path if we didn't already.  */
+	if (!from_sigsys) {
+		input = peek_reg(tracee, ORIGINAL, SYSARG_1);
+		status = read_path(tracee, path, input);
+		if (status < 0)
+			return -errno;
+	}
+
+	/* Check if chroot target has bind mounts inside.
+	 * (Those are not supported currently).  */
+	Binding *binding;
+	for (binding = CIRCLEQ_FIRST(tracee->fs->bindings.guest);
+	     binding != (void *) tracee->fs->bindings.guest;
+	     binding = CIRCLEQ_NEXT(binding, link.guest)) {
+
+		bool is_guest_root = binding == CIRCLEQ_LAST(tracee->fs->bindings.guest);
+
+		if (!is_guest_root && compare_paths(path_guest, binding->guest.path) == PATH1_IS_PREFIX) {
+			seen_bind_under_new_root = true;
+			break;
+		}
+	}
+
+	if (!seen_bind_under_new_root) {
+		/* Change tracee root binding.  */
+		tracee->fs = talloc_zero(tracee, FileSystemNameSpace);
+		binding = new_binding(tracee, path_host_absolute, "/", true);
+		initialize_bindings(tracee);
+		/* Force success.  */
+		if (from_sigsys) return 1;
+		poke_reg(tracee, SYSARG_RESULT, 0);
+		return 0;
+	}
+
+	/* Unsupported chroot() variant.  */
+	return from_sigsys ? -ENOSYS : 0;
 }
