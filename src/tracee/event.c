2f15e0aeaa6cd468cbe7f7b61676ab345f703a4d
Michal Bednarski <bednarski.michal2@gmail.com>
2019-08-14 21:48:04 +0200

    Skip SIGSYS on void syscall when using new syscall order

--- src/tracee/event.c
+++ src/tracee/event.c
@@ -640,7 +640,7 @@ int handle_tracee_event(Tracee *tracee, int tracee_status)
 			siginfo_t siginfo = {};
 			ptrace(PTRACE_GETSIGINFO, tracee->pid, NULL, &siginfo);
 			if (siginfo.si_code == SYS_SECCOMP) {
-				if (tracee->skip_next_seccomp_signal) {
+				if (tracee->skip_next_seccomp_signal || (seccomp_after_ptrace_enter && siginfo.si_syscall == 0xFFFFFFFF)) {
 					VERBOSE(tracee, 4, "suppressed SIGSYS after void syscall");
 					tracee->skip_next_seccomp_signal = false;
 					signal = 0;
