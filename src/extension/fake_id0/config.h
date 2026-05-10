27bfe756c3700cda1ba26e6015fdde4888ade19b
Sylirre <sylirre@termux.dev>
2026-05-10 14:36:00 +0300

    fake_id0: always allow credential changes when started as fake root
    Track initial_euid at setup time. When initial_euid == 0 (proot -0),
    always allow setresuid/setresgid/setreuid/setregid/setuid/setgid/
    setfsuid/setfsgid regardless of the current fake euid, because proot -0
    permanently grants CAP_SETUID/CAP_SETGID semantics.

--- src/extension/fake_id0/config.h
+++ src/extension/fake_id0/config.h
@@ -15,6 +15,14 @@ typedef struct {
 	gid_t fsgid;
 
 	mode_t umask;
+
+	/* Effective UID at the time this fake_id0 config was created.  When
+	 * non-zero, it means the process was spawned with a specific fake
+	 * identity.  When zero (proot -0), the process started as fake root
+	 * and credential changes must always be allowed regardless of the
+	 * current fake euid, because proot -0 permanently grants
+	 * CAP_SETUID/CAP_SETGID semantics. */
+	uid_t initial_euid;
 } Config;
 
 #endif /* FAKE_ID0_CONFIG_H */
